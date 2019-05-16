e502827a3af76f0d549b714deeaf8d75a1cdc25d
Bas <Bas.ghosts@gmail.com>
2019-05-16 11:35:21 +0200

    16-5-19 update

---
+++ BuildingPlayfullWorlds_2/Assets/_Scripts/AttackStats.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class AttackStats : MonoBehaviour
+{
+    [SerializeField]
+    public float Damage;
+    [SerializeField]
+    public float AttackSpeed;
+    [SerializeField]
+    public float AttackRange;
+    [SerializeField]
+    public KeyCode KeyButton;
+}
